f3c029ff0ceaec7a883e34df643eb47a689c2996
perryjhonz <c23daniel.perry@usafa.edu>
2021-03-09 21:13:05 -0700

    commit on 'master'

--- rpnCalc.c
+++ rpnCalc.c
@@ -13,6 +13,10 @@
 // what includes do we need here?
 
 #include "rpnUtils.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
 /* int main() {
     return 0;  // return success
--- rpnUtils.h
+++ rpnUtils.h
@@ -19,4 +19,24 @@
 
 // prototypes for utility functions
 
+/** inputToken()
+ * @brief - collects input from user and places it into a queue
+ * @param queue - a ptr for the queue used in the function
+ * @param input - the user's input
+ * @return - void, because all inputs are in a queue
+ * @pre   - none
+ * @post  - queue has user input, until a NULL character is entered
+ */
+void inputToken(QueueAsLinkedList* queue, char input[50]);
+
+/** inputToken()
+ * @brief - traverses the queue and completes RPN calculations
+ * @param queue - a ptr for the queue used in the function
+ * @param stack - a ptr for the stack used in the function
+ * @return - void
+ * @pre   - must have values and operations gotten from the queue
+ * @post  - calculation done, queue should be empty, and stack only has 1 value
+ */
+void rpnCalculation(QueueAsLinkedList* queue, StackAsLinkedList* stack);
+
 #endif  // RPNUTILS_H
